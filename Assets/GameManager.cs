d2521c8bba4636c4ab8f31cfe91caad40b053194
zanthous <bcm023@gmail.com>
2020-10-03 13:58:11 -0400

    day1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject levelObj;
+    [SerializeField] private GameObject playerObj;
+    private Player player;
+
+    [SerializeField] private AnimationCurve jumpCurve;
+
+    public static bool direction = false;
+    public static bool flipped = false;
+
+    [SerializeField] private float rotationSpeed = 5.0f;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    void Update()
+    {
+        //rotate level
+        var rotationAmount = direction ? Vector3.forward : Vector3.back;
+        rotationAmount = rotationAmount * Time.deltaTime * rotationSpeed;
+        levelObj.transform.Rotate(rotationAmount, Space.World);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float groundedDistance =.075f;
+    [SerializeField] private AnimationCurve jumpCurve;
+    private Rigidbody2D rb;
+
+    private bool jumping = false;
+    [SerializeField] private float jumpForce = 1.0f; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Debug.Log(IsGrounded());
+
+        if(!jumping && Input.GetKeyDown(KeyCode.W) && IsGrounded())
+        {
+            //todo jump 
+            //StartCoroutine(Jump());
+            rb.AddForce((GameManager.flipped ? Vector2.down : Vector2.up) * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private IEnumerator Jump()
+    {
+        jumping = true;
+        float timer = 0.0f;
+        while(timer < jumpCurve.length)
+        {
+            rb.AddForce(Vector2.up * jumpCurve.Evaluate(timer) * jumpForce);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        jumping = false;
+    }
+
+    private bool IsGrounded()
+    {
+        var hits = Physics2D.RaycastAll(transform.position, GameManager.flipped ? Vector2.up : Vector2.down, groundedDistance);
+        Debug.DrawRay(transform.position, (GameManager.flipped ? Vector2.up : Vector2.down) * groundedDistance, Color.blue);
+
+        foreach(var hit in hits)
+        {
+            if(hit.collider != null && hit.collider.transform.tag == "Walkable")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
